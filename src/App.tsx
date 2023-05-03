b6edf5c1a8dd003e081c53b7f5a1ed95a151ad9b
nech <sowul789@gmail.com>
2023-05-03 21:44:36 +0300

    cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
       <h1>One Piece Card</h1>
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Deck } from '../components/home/Cards'
+
+const Home = () => {
+  return (
+    <div>
+        <Deck />
+    </div>
+  )
+}
+
+export default Home
