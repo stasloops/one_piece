ddb2a47d41ab800b498b79cf3377dd7cb1cc8421
nech <sowul789@gmail.com>
2023-05-03 19:47:40 +0300

    finally

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,25 +1,29 @@
-const CACHE_NAME = 'version-1'
-const assetsUrls = [
+const CACHE_NAME = 'version-3'
+const assetUrls = [
     'index.html',
-    // '/index.js',
 ]
+
+
+async function cacheFirst(request) {
+    const cached = await caches.match(request)
+    return cached ?? await fetch(request)
+}
 
 this.addEventListener('install', async (event) => {
     const cache = await caches.open(CACHE_NAME)
-    await cache.addAll(assetsUrls)
+    await cache.addAll(assetUrls)
 })
 
-this.addEventListener('activate', (event) => {
-    // console.log('[SW]: activate');
+this.addEventListener('activate', async (event) => {
+    const cacheNames = await caches.keys()
+    await Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+            .map(name => caches.delete(name))
+    )
 })
-
 
 
 this.addEventListener('fetch', (event) => {
     event.respondWith(cacheFirst(event.request))
 })
 
-async function cacheFirst(request) {
-    const cached = await caches.match(request)
-    return cached ?? await fetch(request)
-}
