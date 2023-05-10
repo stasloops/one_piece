9116349a23e05bea1562ddc4578de6cfbce6537f
nech <sowul789@gmail.com>
2023-05-11 01:30:30 +0300

    h

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import Menu from "./components/menu/Menu";
 import Pages from "./pages/Pages";
 import { useSpring } from "@react-spring/web";
 import { UserProvider } from "./hooks/useUser";
-
+import {FpsView} from "react-fps";
 function App() {
   const [props, api] = useSpring(() => ({ from: { left: "-200vw" } }));
 
   return (
     <div className="app">
+      <FpsView/>
       <UserProvider>
         <Top />
         <Pages props={props} />
